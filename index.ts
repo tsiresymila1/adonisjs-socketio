88a8df2464b1ae4da7d66993596c210e2ad706a7
Tsiresy Milà <tsiresymila@gmail.com>
2024-10-08 10:40:35 +0300

    [update] readme

--- index.ts
+++ index.ts
@@ -8,3 +8,6 @@
 */
 
 export { configure } from './configure.js'
+export { defineConfig } from './src/define_config.js'
+export { stubsRoot } from './stubs/main.js'
+export { OnMessage, OnceMessage } from './src/decorator/handle_message.js'
